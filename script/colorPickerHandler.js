75443c2169b2ac3176eb67d88f40ca7f2543b6ae
Maxime Dupont <emixamdupont@gmail.com>
2020-11-18 20:50:22 +0100

    add bead collection inside color picker

--- script/colorPickerHandler.js
+++ script/colorPickerHandler.js
@@ -1,49 +1,27 @@
 class ColorPickerHandler {
     constructor(htmlIdHook) {
-        this.htmlContainer = document.getElementById(htmlIdHook);
-        this.colorPickerObject = ColorPicker(this.htmlContainer, (hex) => this.handleColorPickerChange(hex));
+        this.container = document.getElementById(htmlIdHook);
+
+        this.container.innerHTML = 
+            `<include src="bead-collection/index.html"></include>
+            <div id="picker" class="cp-default"></div>`
+
+        this.pickerContainer = document.getElementById('picker');
+
+        this.colorPickerObject = ColorPicker(this.pickerContainer, (hex) => this.handleColorPickerChange(hex));
         this.isOpen = false;
 
-        this.initMouseDragElement()
-        // this.htmlContainer.style.left = (window.innerWidth - 260 - 50) + 'px'
-        // this.htmlContainer.style.top = (window.innerHeight - 220 - 50) + 'px'
+        // after mouse enter the container once, a class is set to opacify when the mouse is leaving the container
+        this.container.addEventListener('mouseenter', e =>
+            addClassname(this.container, 'color-picker-opacity')
+        )
 
         window.addEventListener('click', e => {
-            if (this.isOpen && !this.htmlContainer.contains(e.target)
+            if (this.isOpen && !this.container.contains(e.target)
                             && !e.target.classList.contains('form-input')){
                 this.handleCloseColorPicker();
             }
         });
-    }
-
-    initMouseDragElement() {
-        this.htmlContainer.addEventListener('mousedown', function (e) {
-            if(e.target.id === 'color-picker') {
-                this.isDown = true;
-                this.offset = [
-                    this.offsetLeft - e.clientX,
-                    this.offsetTop - e.clientY
-                ];
-            }
-            console.log(e.target.id)
-            console.log(e.target.id === 'color-picker')
-        }, true)
-
-        this.htmlContainer.addEventListener('mouseenter', e =>
-            addClassname(this.htmlContainer, 'color-picker-opacity')
-        )
-
-        document.addEventListener('mouseup', () => {
-            this.htmlContainer.isDown = false;
-        }, true);
-
-        document.addEventListener('mousemove', event => {
-            if (this.htmlContainer.isDown) {
-                event.preventDefault();
-                this.htmlContainer.style.right = (window.innerWidth - event.clientX - this.htmlContainer.offset[0] - 260) + 'px';
-                this.htmlContainer.style.bottom  = (window.innerHeight - event.clientY - this.htmlContainer.offset[1] - 220) + 'px';
-            }
-        }, true);
     }
 
     handleColorPickerChange(hex) {
@@ -55,27 +33,15 @@ class ColorPickerHandler {
     handleOpenColorPicker(key, color) {
         this.key = key;
         this.hex = color;
-        this.htmlContainer.removeAttribute('hidden');
+        this.container.removeAttribute('hidden');
         this.colorPickerObject.setHex(this.hex);
         setTimeout(() => this.isOpen = true, 1);
-
-        this.htmlContainer.addEventListener('mouseover', function() {
-        console.log('Event triggered');
-        });
-
-        var event = new MouseEvent('mouseover', {
-        'view': window,
-        'bubbles': true,
-        'cancelable': true
-        });
-
-        this.htmlContainer.dispatchEvent(event);
     }
 
     handleCloseColorPicker() {
         this.isOpen = false;
         this.key = undefined;
-        this.htmlContainer.setAttribute('hidden', 'true');
-        removeClassname(this.htmlContainer, 'color-picker-opacity')
+        this.container.setAttribute('hidden', 'true');
+        removeClassname(this.pickerContainer, 'color-picker-opacity')
     }
 }
